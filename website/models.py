a96349cd0facfd76ca77d8a96234cf9b168b6736
s02944ys <yashpal.sangha@student.manchester.ac.uk>
2021-02-09 17:47:39 +0000

    Fixed error

--- website/models.py
+++ website/models.py
@@ -1,8 +1,8 @@
-from . import  db
+from . import login_manager, db
 from datetime import datetime
 from flask_login import UserMixin
 
-#@login_manager.user_loader
+@login_manager.user_loader
 def load_user(user_id):
 	return User.query.get(int(user_id))
 
