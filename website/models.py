b9dfcc36d7a37dd6478686dc3151195435c63b84
s02944ys <yashpal.sangha@student.manchester.ac.uk>
2021-03-06 17:23:05 +0000

    Added a movie table and added title to blacklist and whitelist

--- website/models.py
+++ website/models.py
@@ -34,20 +34,36 @@ class Response(db.Model):
 
 class Blacklist(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
+	title = db.Column(db.String(20), nullable=False)
 	user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-	movie_id = db.Column(db.Integer, nullable=False)
+	movie_id = db.Column(db.String(20), nullable=False)
 
 class Whitelist(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
+	title = db.Column(db.String(20), nullable=False)
 	user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-	movie_id = db.Column(db.Integer, nullable=False)
+	movie_id = db.Column(db.String(20), nullable=False)
 
 class Review(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
 	user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-	movie_id = db.Column(db.Integer, nullable=False)
+	movie_id = db.Column(db.String(20), nullable=False)
 	data = db.Column(db.String(10000), nullable=False)
 	date = db.Column(db.DateTime(timezone=True), default=datetime.now())
 	rating = db.Column(db.Float, nullable=False)
 	spoiler_tag = db.Column(db.Boolean)
 
+class Movies(db.Model):
+	movie_id = db.Column(db.String(20), primary_key=True)
+	title = db.Column(db.String(20), nullable=False)
+	rating = db.Column(db.Float, nullable=False)
+	poster = db.Column(db.String(20), nullable=False)
+	genres = db.Column(db.String(50), nullable=False)
+	year = db.Column(db.Integer, nullable=False)
+	plot = db.Column(db.String(100000), nullable=False)
+	actors = db.Column(db.String(10000), nullable=False)
+	directors = db.Column(db.String(10000), nullable=False)
+	runtime = db.Column(db.String(20), nullable=False)
+	trailer = db.Column(db.String(100))
+	language = db.Column(db.String(20))
+	awards = db.Column(db.String(100), nullable=False)
